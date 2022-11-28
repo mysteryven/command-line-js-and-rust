2b29e6519ac1242b1ffa36a8c037f462ffd9de9a
wenzhe <mysteryven@gmail.com>
2022-11-28 20:35:20 +0800

    feat: init cut

---
+++ src/rust/cutr/src/lib.rs
@@ -0,0 +1,34 @@
+use crate::Extract::*;
+use clap::{App, Arg};
+use std::{error::Error, ops::Range};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+type PositionList = Vec<Range<usize>>;
+
+#[derive(Debug)]
+pub enum Extract {
+    Fields(PositionList),
+    Bytes(PositionList),
+    Chars(PositionList),
+}
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    delimiter: u8,
+    extract: Extract,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("cutr")
+        .version("0.1.0")
+        .author("mysteryven <mysteryven@gmail.com>")
+        .about("Rust cut")
+        // What goes here?
+        .get_matches();
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", &config);
+    Ok(())
+}
